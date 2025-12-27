f4baa6dd25477d622c7b1edc8ea178bfe9298bb1
Juan-Ceballos <55723135+Juan-Ceballos@users.noreply.github.com>
2025-12-27 06:58:34 -0500

    method, field, inheritance

---
+++ src/main/FeatureEncapsulation/Student.java
@@ -0,0 +1,13 @@
+package src.main.FeatureEncapsulation;
+
+public class Student {
+    String name;
+    
+    public Student(String name, int age, int grade, int id) {
+        
+    }
+
+    public String getName() {
+        return name;        
+    }
+}
