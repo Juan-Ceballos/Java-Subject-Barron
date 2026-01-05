7d7881104f72112e6842d5f686c6dbad7a9c1a94
Juan-Ceballos <55723135+Juan-Ceballos@users.noreply.github.com>
2026-01-05 00:23:38 -0500

    if statement

--- src/main/FeatureEncapsulation/BankAccount.java
+++ src/main/FeatureEncapsulation/BankAccount.java
@@ -2,8 +2,16 @@ package src.main.FeatureEncapsulation;
 
 public class BankAccount {
     public double myBalance;
+    public boolean pendingSuspension;
     
     public BankAccount() {
         myBalance = 0.0;
     }
+
+    boolean checkSuspension() {
+        if (myBalance < -50) {
+            pendingSuspension = true;
+        }
+        return pendingSuspension;
+    }    
 }
