403bf363e8272ac8d3af0ca5d2343326273d15a1
Juan-Ceballos <55723135+Juan-Ceballos@users.noreply.github.com>
2026-01-07 01:05:19 -0500

    array list

--- src/main/FeaturesJApplication.java
+++ src/main/FeaturesJApplication.java
@@ -1,12 +1,16 @@
 package src.main;
 
+import java.util.ArrayList;
+
 import src.main.FeatureEncapsulation.BankAccount;
+import src.main.FeatureEncapsulation.Student;
 
 public class FeaturesJApplication {
     public static void main(String[] args) {
         System.out.println("Hello");
         BankAccount juanBankAccount = new BankAccount();
         System.out.println(juanBankAccount.myBalance);
+        ArrayList<Student> studentList = new ArrayList<Student>();
     }
     
 }
