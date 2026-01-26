10e89cb0e6a150a65f2bad7ea67d351ee7b18a76
Juan-Ceballos <55723135+Juan-Ceballos@users.noreply.github.com>
2026-01-25 22:21:59 -0500

    barron ch2 classes, use of toString and private vars in class

---
+++ src/main/FeatureEncapsulation/Time.java
@@ -0,0 +1,23 @@
+package src.main.FeatureEncapsulation;
+
+public class Time {
+    private int myHrs;
+    private int myMins;
+    private int mySecs;
+
+    public Time() {
+
+    }
+
+    public Time(int h, int m, int s) {
+        myHrs = h;
+        myMins = m;
+        mySecs = h;
+    }
+
+    public String toString() {
+        return "";
+    }
+}
+
+
--- src/main/FeaturesJApplication.java
+++ src/main/FeaturesJApplication.java
@@ -4,13 +4,26 @@ import java.util.ArrayList;
 
 import src.main.FeatureEncapsulation.BankAccount;
 import src.main.FeatureEncapsulation.Student;
+import src.main.FeatureEncapsulation.Time;
 
 public class FeaturesJApplication {
+    public void display(Time t) {
+        /*
+        Wrong Answer
+        printing t.myHrs, etc.
+        Time class has private fields, and a 
+        to string method that allows printing of 
+        class based on the format of method implementation
+        */
+        System.out.println(t);
+    }
     public static void main(String[] args) {
         System.out.println("Hello");
         BankAccount juanBankAccount = new BankAccount();
         System.out.println(juanBankAccount.myBalance);
         ArrayList<Student> studentList = new ArrayList<Student>();
+
+        
     }
     
 }
